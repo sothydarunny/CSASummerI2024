56667c17e5dec16d4e6f9682a74f2fe1d47a999e
altradCS <145883008+altradCS@users.noreply.github.com>
2024-05-20 13:02:31 +0700

    Update and rename processString.py to githublab1.py

---
+++ githublab1.py
@@ -0,0 +1,9 @@
+
+
+
+
+
+# Prompt user to enter the minutes (e.g., 1 billion), and displays the number of years and days for the minutes. 
+# Sample run:
+# Enter the number of minutes:  1000000000
+# 1000000000 minutes is approximately 1902 years and 214 days 
