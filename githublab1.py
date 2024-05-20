1f4b04047e1d3696177ac41f0889cea54824467f
LAH <abdaltrad@yahoo.com>
2024-05-20 13:55:24 +0700

    done

--- githublab1.py
+++ githublab1.py
@@ -1,4 +1,5 @@
-original_text = "aupp"
-doubled_text = ........................
-print(f"Original text: {original_text}")
-print(f"Doubled text: {doubled_text}")
+original_text="AUPP"
+doubled_text= "".join([char * 2 for char in original_text])
+print(f"Original text:{original_text}")
+print(f"Doubled text:{doubled_text}")
+
