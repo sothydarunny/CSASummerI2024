07225ddfa3850fe28d56f8e2d83d7ad37813e7ad
LAH <abdaltrad@yahoo.com>
2024-05-20 12:10:02 +0700

    done

--- processString.py
+++ processString.py
@@ -1,5 +1,5 @@
 # Double each letter in the original text
 original_text = "AUPP"
-doubled_text = .....
+doubled_text = "".join([char * 2 for char in original_text])
 print(f"Original text: {original_text}")
 print(f"Doubled text: {doubled_text}")
