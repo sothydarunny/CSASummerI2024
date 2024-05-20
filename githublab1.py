0bab301da4b418aca76f8f35d6629421fa44fba5
altradCS <145883008+altradCS@users.noreply.github.com>
2024-05-20 13:10:57 +0700

    Update githublab1.py

--- githublab1.py
+++ githublab1.py
@@ -1,9 +1,4 @@
-
-
-
-
-
-# Prompt user to enter the minutes (e.g., 1 billion), and displays the number of years and days for the minutes. 
-# Sample run:
-# Enter the number of minutes:  1000000000
-# 1000000000 minutes is approximately 1902 years and 214 days 
+original_text = "aupp"
+doubled_text = ........................
+print(f"Original text: {original_text}")
+print(f"Doubled text: {doubled_text}")
